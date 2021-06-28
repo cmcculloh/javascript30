58f0bd8c7db42341b665b1b005f143cf55988bed
Christopher McCulloh <cmcculloh@salesforce.com>
2021-06-28 10:54:06 -0400

    play/stop audio appropriately.

--- 1-drum-kit/src/util/Key.tsx
+++ 1-drum-kit/src/util/Key.tsx
@@ -1,3 +1,5 @@
+import {useEffect, useRef} from 'react';
+
 interface KeyProps {
     isPlaying: boolean,
     letter: string,
@@ -6,10 +8,28 @@ interface KeyProps {
 }
 
 const Key:React.FC<KeyProps> = ({ isPlaying, letter, toggleSound, sound }) => {
+    const savedSound = useRef<string>(sound);
+    const savedAudio = useRef<HTMLAudioElement>(new Audio(`sounds/${sound}.wav`));
+ 
+    useEffect(() => {
+        if (savedSound.current !== sound) {
+            savedAudio.current = new Audio(`sounds/${sound}.wav`);
+            savedSound.current = sound;
+        }
+      }, [sound]);
+ 
+    if (isPlaying) {
+        savedAudio?.current?.play();
+    } else {
+        savedAudio.current.pause();
+        savedAudio.current.currentTime = 0;
+    }
+    
     return (
     <div data-key={letter} className={`key ${isPlaying ? 'playing' : ''}`} onClick={() => toggleSound(letter)} >
         <kbd>{letter}</kbd>
         <span className="sound">{sound}</span>
+        <audio src="sounds/{sound}.wav" preload="auto"></audio>
     </div>
     )
 }
