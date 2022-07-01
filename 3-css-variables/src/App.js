bebede6f986e164ac0561b4043876dcd5769f610
cmcculloh <cmcculloh@gmail.com>
2022-07-01 12:32:35 -0400

    Adds starter HTML

--- 3-css-variables/src/App.js
+++ 3-css-variables/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>
+				Update CSS Variables with <span className="hl">JS</span>
+			</h2>
+
+			<div className="controls">
+				<label for="spacing">Spacing:</label>
+				<input
+					id="spacing"
+					type="range"
+					name="spacing"
+					min="10"
+					max="200"
+					value="10"
+					data-sizing="px"
+				></input>
+
+				<label for="blur">Blur:</label>
+				<input
+					id="blur"
+					type="range"
+					name="blur"
+					min="0"
+					max="25"
+					value="10"
+					data-sizing="px"
+				></input>
+
+				<label for="base">Base Color</label>
+				<input id="base" type="color" name="base" value="#ffc600"></input>
+			</div>
+
+			<img alt="something to blur" src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
+		</div>
+	);
 }
 
 export default App;
